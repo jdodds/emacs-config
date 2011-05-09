8fffca330d93821625d696578e6b77b31607cfe0
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2011-05-09 14:05:57 -0400

    update install script for OSX compatibility

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
-LOGFILE=$(readlink -f .)/install.log
+
+function find_path () {
+    readlink="readlink"
+    if [[ $(uname) == "Darwin" ]]; then
+        PATH=$PATH:/opt/local/bin
+        readlink="greadlink"
+    fi
+    $readlink -f "$1"
+}
+
+LOGFILE=$(find_path .)/install.log
 
 function log_command () {
     $1 | tee -a $LOGFILE
@@ -10,19 +20,19 @@ if [[ ! -f /usr/local/share/emacs/site-lisp/magit.el || $1 == "rebuild" ]]; then
     log_command "make" &&  log_command "sudo make install"
 fi
 
-to_link=(.emacs .emacs.d foo)
+to_link=(.emacs .emacs.d)
 
 # symlink everything in $to_link to $(HOME) that isn't already linked, and make
 # a diff file and $link.new file if something else is sitting there.
 for link in ${to_link[@]}; do
     target=$HOME/$link
-    original=$(readlink -f $link)
+    original=$(find_path $link)
 
     if [ ! -L $target ]; then
 	if [[ -f $target && $(diff $original $target) ]]; then
 	    newtarget=$target.new
 	    difftarget=$target.diff
-	    echo "installing $target as $newtarget, check $(readlink -f $difftarget)"
+	    echo "installing $target as $newtarget, check $(find_path $difftarget)"
 	    diff -u $original $target > $difftarget
 	    target=$target.new
 	fi
