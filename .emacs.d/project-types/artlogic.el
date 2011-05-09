b3804709f7fb7b4fcb240daae0d6b66440e6c792
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2011-05-09 19:39:49 -0400

    add dir-locals.el to artlogic project definition

--- .emacs.d/project-types/artlogic.el
+++ .emacs.d/project-types/artlogic.el
@@ -1,5 +1,13 @@
 (require 'startproject)
 
+(setq dir-local-def
+      (prin1-to-string
+       '((nil \. ((indent-tabs-mode \. nil)
+		(tab-width \. 3)))
+	  (espresso-mode \. ((espresso-indent-level  \. 3)))
+	  (js-mode \. ((js-indent-level \. 3))))
+       t))
+
 (startproject-add-commands
  "artlogic"
  "mkdir trunk"
@@ -7,4 +15,7 @@
  "mkdir trunk/AlDocs"
  "touch trunk/AlDocs/ReadMe.txt"
  "echo '// Copyright (c)' $(date +%Y) 'Art & Logic Software Development, Inc' >> trunk/AlDocs/ReadMe.txt"
+ (concat "echo '" dir-local-def "' > .dir-locals.el")
  "echo '// $id$' >> trunk/AlDocs/ReadMe.txt")
+
+(provide 'artlogic)
