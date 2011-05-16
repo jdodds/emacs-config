962a0182aad67e9938095d14156c550e24459678
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2011-05-16 17:31:26 -0400

    add vm and ignores for it's bzr repo

---
+++ vm/release.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# -*- shell-script -*-
+
+. ./getversion.sh
+
+
+# now check for uncommitted changes
+if [ "$1" != "test" ]; then 
+  bzr diff || exit 1
+fi
+
+# check for an error less build
+if [ "$1" != "test" ]; then 
+  # make sure we delete the existing files containing version info
+  rm -f lisp/version.txt info/version.texi
+  make all || exit 1
+fi
+
+# just create the version-info, no bundle 
+if [ "$1" == "version-info" ]; then 
+  exit 0
+fi
+
+dir="release/$rdir"
+rm -rf $dir
+mkdir -p release
+$bzr export $dir
+
+cp configure $dir
+# The following lines are useless and misguided.  USR, 2010-03-13
+# mv lisp/version.txt $dir/lisp
+# mv info/version.texi $dir/info
+rm $dir/getversion.sh $dir/release.sh
+cd release
+tar cvfz $rdir.tgz $rdir
+cd ..
+
+if [ -n "$1" -a -e "$1" ]; then
+  ./$1 $dir.tgz $nick $revno
+fi
+
+echo "Remember to push revisions!!!"
+# if [ "$1" != "test" ]; then 
+#   $bzr push --overwrite
+# fi
---
+++ vm/vm-load.el
@@ -0,0 +1,6 @@
+; -*- mode: emacs-lisp -*-
+;; Load VM easily
+
+(add-to-list 'load-path "/home/jdd/.unix-confs/emacs-config/vm/lisp")
+
+(load "vm-autoloads")
