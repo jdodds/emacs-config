b63753f3412f26cb3ecdd947ef229590e873f2f4
Jeremiah Dodds <jeremiah.dodds@gmail.com>
2012-02-16 03:57:43 -0500

    elpa updates

---
+++ .emacs.d/elpa/org-20120208/org-pkg.el
@@ -0,0 +1,1 @@
+(define-package "org" "20120208" "Outline-based notes management and organizer" nil)
---
+++ .emacs.d/elpa/sauron-0.2/sauron-pkg.el
@@ -0,0 +1,2 @@
+(define-package "sauron" "0.2"
+  "Track (erc/org/dbus/...) events and react to them.")
